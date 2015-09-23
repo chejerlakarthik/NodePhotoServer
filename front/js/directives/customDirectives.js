741890b7ada7fcc97f0fcf6964dc977ba3c905a2
Chejerla Karthik <chejerlakarthik@gmail.com>
2015-09-23 09:03:48 +0530

    Added custom directives to set css for page footer

--- front/js/directives/customDirectives.js
+++ front/js/directives/customDirectives.js
@@ -15,3 +15,34 @@ album.directive('newAlbumForm',function(){
 		templateUrl: '/partials/new_album_form.html'
  	}
 });
+
+album.directive('photoAlbumFooter', function() {
+	return {
+		restrict : 'A',
+		templateUrl : '/partials/photo_album_footer.html',
+		link : function(scope, element, attrs) {
+			element.css('outline', '1px solid red');
+			element.css('position', 'fixed');
+			element.css('bottom', '0px');
+			element.css('width', '100%');
+			element.css('margin', 'auto');
+			element.css('height', '30px');
+			element.css('background-color', '#f5f5f5');
+		}
+	}
+});
+
+album.directive('footerContent', function() {
+	return {
+		restrict : 'A',
+		link : function(scope, element, attrs) {
+
+			element.css('margin', 'auto');
+			element.css('width', '60%');
+			element.css('color', 'navy');
+			element.css('padding-top', '10px');
+			element.css('padding-bottom', '10px');
+			element.css('padding-left', '100px');
+		}
+	}
+});
